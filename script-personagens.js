b82680609224d999a121be1681416b7731106170
capprinha <gutoklein21@gmail.com>
2024-12-04 19:37:18 -0300

    add john

--- script-personagens.js
+++ script-personagens.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
+    //arthur
     $('#arthur-h1').click(function(){
-        $('#arthur-main').slideToggle();
+        $('#arthur-main').slideToggle()
+
+        const destinoArthur = $('.arthur-conteudo')
+
+        $('html').animate({
+            scrollTop: destinoArthur.offset().top
+        }, 1000);
+        });
+
+    // john
+    $('#john-h2').click(function(){
+        $('#john-main').slideToggle()
+
+        const destinoJohn = $('.john-conteudo')
+        
+        $('html').animate({
+            scrollTop: destinoJohn.offset().top
+        },1000);
     });
+
+    
 });
